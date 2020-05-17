13b33590b907ce1ec03d8f73efb2bb370061397a
Khalid Elshafie <abolkog@gmail.com>
2020-05-18 00:26:01 +1000

    Deploying to heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
-
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is ready for connections on port ${PORT}`);
+  console.log(`Server is ready for connections on port ${PORT}`);
 });
-
--- server/src/app.js
+++ server/src/app.js
@@ -4,22 +4,23 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
+const path = require('path');
 
 const v1 = require('./routes/v1');
 
 const app = express();
 
 // ----------- DB Config -----------//
-mongoose.connect(process.env.MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
 });
 
 mongoose.connection.on('connected', () => {
-    console.log('Connected to the database');
+  console.log('Connected to the database');
 });
 mongoose.connection.on('error', (err) => {
-    console.error(`Failed to connected to the database: ${err}`);
+  console.error(`Failed to connected to the database: ${err}`);
 });
 
 // ----------- Middlewares -----------//
@@ -36,25 +37,32 @@ require('./config/passport')(passport);
 // ----------- Routes -----------//
 app.use('/api/v1', v1);
 
+// ----------- Static Files -----------//
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../../client', 'build', 'index.html')
+    );
+  });
+}
 
 // ----------- ERRORS -----------//
 
-app.use((req, res, next) => { //404 Not Found
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use((req, res, next) => {
+  //404 Not Found
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 app.use((err, req, res, next) => {
-    const status = err.status || 500;
-    const error = err.message || 'Error processing your request';
+  const status = err.status || 500;
+  const error = err.message || 'Error processing your request';
 
-    res.status(status).send({
-        error
-    })
+  res.status(status).send({
+    error,
+  });
 });
-
-
-
 
 module.exports = app;
